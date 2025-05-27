7e2d778b2aa622739000c12fea93f2a96986296a
Roman <41762401+Source34@users.noreply.github.com>
2025-05-27 19:35:13 +0300

    Task1: Add tree helper implementation
    - add Node type
    - add TreeHelper.Swap()

---
+++ DataStructures/DataStructures/Trees/Node.cs
@@ -0,0 +1,36 @@
+﻿namespace DataStructures.Trees;
+
+/// <summary>
+/// Представляет узел бинарного дерева.
+/// </summary>
+public class Node
+{
+    /// <summary>
+    /// Значение, хранящееся в узле.
+    /// </summary>
+    public int Value { get; set; }
+
+    /// <summary>
+    /// Левый потомок.
+    /// </summary>
+    public Node? Left { get; set; }
+
+    /// <summary>
+    /// Правый потомок.
+    /// </summary>
+    public Node? Right { get; set; }
+
+    /// <summary>
+    /// Создаёт узел с заданным значением.
+    /// </summary>
+    /// <param name="value">Значение узла.</param>
+    public Node(int value)
+    {
+        Value = value;
+    }
+
+    /// <summary>
+    /// Cтроковое представление.
+    /// </summary>
+    public override string ToString() => Value.ToString();
+}
---
+++ DataStructures/DataStructures/Trees/TreeHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructures.Trees;
+
+/// <summary>
+/// Содержит алгоритмы работы с бинарными деревьями.
+/// </summary>
+public static class TreeHelper
+{
+    /// <summary>
+    /// Зеркально отражает бинарное дерево по вертикальной оси.
+    /// </summary>
+    /// <param name="tree">Корень дерева для отражения.</param>
+    /// <returns>Корень отражённого дерева.</returns>
+    public static Node? Swap(Node? tree)
+    {
+        if (tree == null) return null;
+
+        var temp = tree.Left;
+        tree.Left = Swap(tree.Right);
+        tree.Right = Swap(temp);
+
+        return tree;
+    }
+}
